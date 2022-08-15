7846c7e507d270ef851ff6252308b3f7876d551f
andersonfranciscoig@gmail.com <andersonfranciscoig@gmail.com>
2022-08-15 14:23:45 +0100

    add rout

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultThemes } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
-import { Button } from './Components/Button'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultThemes}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="sucess" />
-      <Button variant="danger" />
-      <Button />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
--- src/Components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainer, ButtoVariant } from './Button.styles'
-
-interface ButtonPorp {
-  variant?: ButtoVariant
-}
-
-export function Button({ variant = 'primary' }: ButtonPorp) {
-  return <ButtonContainer variant={variant}>Enviar</ButtonContainer>
-}
